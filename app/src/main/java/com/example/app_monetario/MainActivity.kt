861c4c25539ebbc21e464bce10eed8413d525391
caiovieira <95643550+caioo1@users.noreply.github.com>
2023-03-04 13:21:18 -0300

    Quase finalizando o tutorial
    Parte funcional e finalização do design. Porém falta testar no emulador e colocar  a URL do site completa que estou esperando no email.

--- app/src/main/java/com/example/app_monetario/MainActivity.kt
+++ app/src/main/java/com/example/app_monetario/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.app_monetario
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var result: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        result = findViewById<TextView>(R.id.txt_result)
+        val buttonConveter = findViewById<Button>(R.id.btn_converter)
+        buttonConverter.setOnClickListener {
+            converter()
+        }
+    }
+
+    private fun converter() {
+        val selectedCurrency = findViewById<RadioGroup>(R.id.radio_group)
+        val checked = selectedCurrency.checkedRadioButtonId
+
+        val currency = when (checked) {
+            R.id.radio_usd -> "USD"
+            R.id.radio_eur -> "EUR"
+            else -> "CLP"
+        }
+        val editField = findViewById<EditText>(R.id.edit_field)
+        val value = editField.text.toString()
+        if (value.isEmpty())
+            return
+        result.text = value
+        result.visibility = View.VISIBLE
+
+        Thread {
+            val url =
+                URL("https://free.currconv.com/api/v7/convert?q=${currency}_BRL&compact=ultra&apiKeyminhakey")
+            val conn = url.openConnection() as HttpsURLConnection
+            try {
+                val data = conn.inputStream.bufferedReader().readText()
+                //{"asdajsd": 1231232}
+                val obj = JSONObject(data)
+                runOnUiThread{
+                    val res = obj.getDouble("${currency}_BRL")
+                    result.text = "R$${"%.4f".format(value.toDouble() * res)}"
+                    result.visibility = View.VISIBLE
+                }
+
+                runOnUiThread {
+                    result.text = data
+                    result.visibility = View.VISIBLE
+                }
+
+            } finally {
+                conn.disconnect()
+            }
+        }.start()
     }
 }
